da886b42c0eb3c432b980e1aa54fce5115c21290
Dhruv Garg <garg.dh123@gmail.com>
2020-05-01 22:22:26 +0530

    improved implementation

--- src/Board.js
+++ src/Board.js
@@ -8,6 +8,7 @@ export default class Board extends React.Component {
   constructor(props) {
     super(props);
     const clients = this.getClients();
+    
     this.state = {
       clients: {
         backlog: clients.filter(client => !client.status || client.status === 'backlog'),
@@ -15,28 +16,14 @@ export default class Board extends React.Component {
         complete: clients.filter(client => client.status && client.status === 'complete'),
       }
     }
+
     this.swimlanes = {
       backlog: React.createRef(),
       inProgress: React.createRef(),
       complete: React.createRef(),
     }
-
-    var drake = Dragula({
-      isContainer: function (el) {
-        return el.classList.contains('Card Card-grey') || el.classList.contains('Card Card-blue') || el.classList.contains('Card Card-green') || el.classList.contains('Swimlane-dragColumn');
-      },
-      accepts: function (el, target, source, sibling) {
-        return target.classList.contains('Swimlane-dragColumn');
-      }
-    });
-
-    drake.on('drop', function (el, target, source, sibling) {
-      el.classList.remove(...el.className.split(' '))
-      el.classList.add(...sibling.className.split(' '))
-      return true;
-    });
-
   }
+
   getClients() {
     return [
       ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'in-progress'],
@@ -70,6 +57,64 @@ export default class Board extends React.Component {
     return (
       <Swimlane name={name} clients={clients} dragulaRef={ref}/>
     );
+  }
+
+  componentDidMount() {
+    this.drake = Dragula([
+      this.swimlanes.backlog.current,
+      this.swimlanes.inProgress.current,
+      this.swimlanes.complete.current,
+    ]);
+    this.drake.on('drop', (el, target, source, sibling) => this.updateClient(el, target, source, sibling));
+  }
+
+  componentWillUnmount() {
+    this.drake.remove();
+  }
+
+  /**
+   * Change the status of client when a Card is moved
+   */
+  updateClient(el, target, _, sibling) {
+    // Reverting DOM changes from Dragula
+    this.drake.cancel(true);
+
+    // Find out which swimlane the Card was moved to
+    let targetSwimlane = 'backlog';
+    if (target === this.swimlanes.inProgress.current) {
+      targetSwimlane = 'in-progress';
+    } else if (target === this.swimlanes.complete.current) {
+      targetSwimlane = 'complete';
+    }
+
+    // Create a new clients array
+    const clientsList = [
+      ...this.state.clients.backlog,
+      ...this.state.clients.inProgress,
+      ...this.state.clients.complete,
+    ];
+    
+    const clientThatMoved = clientsList.find(client => client.id === el.dataset.id);
+    const clientThatMovedClone = {
+      ...clientThatMoved,
+      status: targetSwimlane,
+    };
+
+    // Remove ClientThatMoved from the clientsList
+    const updatedClients = clientsList.filter(client => client.id !== clientThatMovedClone.id);
+
+    // Place ClientThatMoved just before the sibling client, keeping the order
+    const index = updatedClients.findIndex(client => sibling && client.id === sibling.dataset.id);
+    updatedClients.splice(index === -1 ? updatedClients.length : index , 0, clientThatMovedClone);
+
+    // Update React state to reflect changes
+    this.setState({
+      clients: {
+        backlog: updatedClients.filter(client => !client.status || client.status === 'backlog'),
+        inProgress: updatedClients.filter(client => client.status && client.status === 'in-progress'),
+        complete: updatedClients.filter(client => client.status && client.status === 'complete'),
+      }
+    });
   }
 
   render() {
